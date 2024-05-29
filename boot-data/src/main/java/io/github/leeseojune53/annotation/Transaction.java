c13cee18bf7ff61bb1a274087c865f9c1fa2098b
Arthur <arthur@jaranda.kr>
2024-05-29 15:22:25 +0900

    Transaction

--- boot-data/src/main/java/io/github/leeseojune53/annotation/Transaction.java
+++ boot-data/src/main/java/io/github/leeseojune53/annotation/Transaction.java
@@ -1,5 +1,7 @@
 package io.github.leeseojune53.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.METHOD;
@@ -9,6 +11,7 @@ import static java.lang.annotation.ElementType.TYPE;
  * 생각해보니 Proxy를 먹일 때 각 클래스를 생성할 때 계산하는 것 보다 해당 조건(어노테이션이 있으면 실행 등)을 충족하면 실행되도록하면 생성 부분이 기존처럼 복잡하지 않고 간단해진다.
  */
 
+@Retention(RetentionPolicy.RUNTIME)
 @Target({
         METHOD,
         TYPE
--- boot-data/src/main/java/io/github/leeseojune53/transaction/TransactionProxy.java
+++ boot-data/src/main/java/io/github/leeseojune53/transaction/TransactionProxy.java
@@ -2,24 +2,39 @@ package io.github.leeseojune53.transaction;
 
 import io.github.leeseojune53.annotation.Transaction;
 import io.github.leeseojune53.aop.AopSpec;
-import io.github.leeseojune53.aop.annotation.Aop;
 
-@Aop(Transaction.class)
+import java.lang.reflect.Method;
+
+@SuppressWarnings("unused")
 public class TransactionProxy implements AopSpec {
 
+    @Override
+    public boolean condition(Method method) {
+        if(method.getAnnotation(Transaction.class) != null)
+            return true;
+
+        if(method.getClass().getAnnotation(Transaction.class) != null)
+            return true;
+
+        return false;
+
+    }
 
     @Override
     public void before() {
         // Transaction 시작
+        System.out.println("Transaction Start");
     }
 
     @Override
     public void after() {
         // Transaction 종료
+        System.out.println("Transaction End");
     }
 
     @Override
     public void error() {
         // Transaction Rollback
+        System.out.println("Transaction Rollback");
     }
 }
